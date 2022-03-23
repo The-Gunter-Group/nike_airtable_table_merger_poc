67fa82d38fe8eb68518c63866ded6cc4df2ec5a1
ao-tgg <andeo@guntergroupconsulting.com>
2022-03-23 15:22:25 -0700

    Add documentation

--- frontend/index.js
+++ frontend/index.js
@@ -5,9 +5,18 @@
  * Merge/Join
  * CRUD for record Data
  * 
+ * A few TODOs should be considered for this POC:
+ *  - Configurability - Consideration should be given to how configurable this process can/should be:
+ *      ~ UI for selecting for Left and Right Tables?
+ *      ~ Dynamically selecting join keys
+ *  - Error Handling:
+ *      ~ For the POC, demonstrating the functionality was within scope but adequate error handling was not.
+ *  - Unit Test
+ *  - Rate limiting:
+ *      ~ Airtable APIs rate limit writes to 50 records a request
+ *      ~ Airtable APIs rate limit reads to 500 records a request
+ *  - Deployment - Deployment to Airtable marketplace was outside of the scope, including use across bases
  *
- * @link   URL
- * @file   This files defines the MyClass class.
  * @author The Gunter Group, attn: andeo@guntergroup.com
  * @since  3.23.2022
  */
@@ -18,14 +27,15 @@
  import alasql from 'alasql';
  var _ = require('lodash');
  
- 
+/** ****************             Table Building/Merging         **************************** */
+
  function joinTables(leftTable, rightTable, leftKey, rightKey, allFields) {
  /**
   * Join two tables given a join key for each.
   *
   * Basic implementation of a join between two tables. This function
   * leverages alasql for performing the join. For some record level details
-  * additional information is not needed and is filtered out
+  * additional information is not needed and is filtered out.
   *
   * @since      3.23.2022
   * @access     public
@@ -58,6 +68,25 @@
  }
  
  function formatJoinableTable(table, recordsRequired=true) {
+ /**
+  * Rework airtable object to create a table that can be joined.
+  *
+  * Airtable objects contain information that can't/doesn't need to 
+  * be included in the data that gets pushed back to the tables. This 
+  * function removes information that shouldn't be included in the joining
+  * process.
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {Object}   table             Airtable table object to transform.
+  * @param {boolean}  recordsRequired   Boolean flag to include records.
+  *
+  *
+  * @return {Array.<Object, Array>} Formatted table and array of fields.
+  */  
+
      var tempTable = [];
      var records = useRecords(table);
      var fieldArray = [];
@@ -65,11 +94,11 @@
          var rowMap = {};
          table.fields.forEach(function(field){
              var cells = record.getCellValue(field);
-             // Remove Ids from objects because IDs aren't relevant in new table
+             // Remove Ids from objects because Airtable IDs aren't relevant in new table
              try {
                  delete cells.id;
              } catch(err) {
-                 //not an object
+                 // not an object
              }
              try {
                  cells.forEach(function(cell) {
@@ -94,13 +123,104 @@
  }
  
  function createTableInDB(table, name) {
+ /**
+  * Create Alasql table given an object and a name.
+  *
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {Object}   table     Object to turn into an alasql table.
+  * @param {string}   name      Name to create for table.
+  *
+  *
+  * @return {boolean}  .
+  */ 
+
      alasql(`DROP TABLE IF EXISTS table_${name.replace(/\s/g, '')}`)
      alasql(`CREATE TABLE IF NOT EXISTS table_${name.replace(/\s/g, '')}`)
      alasql(`SELECT * INTO table_${name.replace(/\s/g, '')} FROM ?`, [table])
-     return true
  }
  
+
+ function joinTablesinDB(sourceTableName, mergedTableName) {
+ /**
+  * Join two tables in Alasql.
+  *
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {string}   sourceTableName      Source table name.
+  * @param {string}   mergedTableName      Name to create for table.
+  *
+  *
+  */ 
+    alasql(`DROP TABLE IF EXISTS ${mergedTableName.replace(/\s/g, '')}`)
+    alasql(`CREATE TABLE IF NOT EXISTS ${mergedTableName.replace(/\s/g, '')}`)
+    alasql(`SELECT * INTO ${mergedTableName.replace(/\s/g, '')} FROM table_source RIGHT JOIN table_${sourceTableName.replace(/\s/g, '')} ON table_source.[Concept Name] = table_${sourceTableName.replace(/\s/g, '')}.[Concept Name]`)
+}
+
+async function createMergedTable(name, allFields, key) {
+ /**
+  * Creates a new merged table in the airtable base.
+  *
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {string}           name          Name of merged table.
+  * @param {Array.<string>}   allFields     Array of fields to include in merged table.
+  * @param {string}           key           Merge key.
+  *
+  *
+  */     
+    const tableFlag = base.getTableByNameIfExists(name);
+
+    const field_array = []
+
+    allFields.forEach(field => {
+        const {id, name, description, type, options, ...tempArray} = field;
+        const fieldMap = {'name': name, 'description': description, 'type': type, 'options': options};
+        try {
+            fieldMap.options.choices.forEach(choice => {
+                delete choice.id;
+            })
+        }
+        catch(err) {
+            // Do nothing for now
+        }
+        let cleaned = Object.fromEntries(Object.entries(fieldMap).filter(([_, v])=>v!=null));
+        field_array.push(cleaned);
+    });
+    const names = field_array.map(o => o.name)
+    const filteredFieldArray = field_array.filter(({name}, index) => !names.includes(name, index+1))
+    filteredFieldArray.sort((x,y)=>{ return x.name === key ? -1 : y.name === key ? 1 : 0; });
+
+    if (base.hasPermissionToCreateTable(name, filteredFieldArray) && !tableFlag) {
+        await base.createTableAsync(name, filteredFieldArray);
+    }
+
+}
+
+ /** ****************             CRUD Operations        **************************** */
  async function insertMergedRecords(mergedTable, name) {
+ /**
+  * Insert Merged Records to new table.
+  *
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {Object}   mergedTable     Object to turn into an alasql table.
+  * @param {string}   name            Name of table to write to.
+  *
+  *
+  */ 
      const table = base.getTableByNameIfExists(name);
      if (mergedTable && mergedTable.length) {
          if (mergedTable) {
@@ -117,6 +237,20 @@
  }
  
  async function deleteStaleRecordsFromTable(staleRecords, mergeTable, name) {
+ /**
+  * Delete stale records from table.
+  *
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {Array.<Object>}   staleRecords     Array of stale records to delete.
+  * @param {Object}           mergeTable       Name of table to write to.
+  * @param {string}           name            Name of table to write to.
+  *
+  *
+  */      
      const table = base.getTableByNameIfExists(name);
      var records = formatJoinableTable(mergeTable)
      if (staleRecords && staleRecords.length) {
@@ -137,14 +271,31 @@
  
  
  function checkRecordsForDups(table, merged) {
+ /**
+  * Check for and identify duplicate records.
+  *
+  * Duplicate records can be created when merging into the merged table
+  * because the UniqueIDs from the source table can't be used in the destination
+  * without including the ID in the view. This function ensures we won't have 
+  * identical records get copied into the merge table more than once.
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {Object}   table     Source table.
+  * @param {Object}   merged    Merged Table.
+  *
+  * @return {Array.<Object>}  Array of duplicated records
+  */ 
      if (!table.length) {
          var resultSet = merged
      } else {
          table.forEach(function(row){
-             delete row.id;
+             delete row.id; // Remove IDs so that we only check row values
          })
          merged.forEach(function(row){
-             delete row.id;
+             delete row.id; // Remove IDs so that we only check row values
          })
          var resultSet = _.differenceWith(merged, table, _.isEqual)
      }
@@ -152,6 +303,21 @@
  }
  
  function findStaleRecords(table, merged) {
+ /**
+  * Check for and identify stale records.
+  *
+  * Users can delete records from the source or mapping table.
+  * This identifies records that were removed.
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  *
+  * @param {Object}   table     Source table.
+  * @param {Object}   merged    Merged Table.
+  *
+  * @return {Array.<Object>}  Array of stale records
+  */      
      if (!table.length) {
          var resultSet = merged
      } else {
@@ -159,46 +325,20 @@
      }
      return resultSet
  }
- 
- async function createMergedTable(name, all_fields, key) {
-     const tableFlag = base.getTableByNameIfExists(name);
- 
-     const field_array = []
- 
-     all_fields.forEach(field => {
-         const {id, name, description, type, options, ...tempArray} = field;
-         const fieldMap = {'name': name, 'description': description, 'type': type, 'options': options};
-         try {
-             fieldMap.options.choices.forEach(choice => {
-                 delete choice.id;
-             })
-         }
-         catch(err) {
-             // Do nothing for now
-         }
-         let cleaned = Object.fromEntries(Object.entries(fieldMap).filter(([_, v])=>v!=null));
-         field_array.push(cleaned);
-     });
-     const names = field_array.map(o => o.name)
-     const filteredFieldArray = field_array.filter(({name}, index) => !names.includes(name, index+1))
-     filteredFieldArray.sort((x,y)=>{ return x.name === key ? -1 : y.name === key ? 1 : 0; });
- 
-     if (base.hasPermissionToCreateTable(name, filteredFieldArray) && !tableFlag) {
-         await base.createTableAsync(name, filteredFieldArray);
-     }
- 
- }
- 
- function joinTablesinDB(source_name, table_name, merged_name) {
-     alasql(`DROP TABLE IF EXISTS ${merged_name.replace(/\s/g, '')}`)
-     alasql(`CREATE TABLE IF NOT EXISTS ${merged_name.replace(/\s/g, '')}`)
-     alasql(`SELECT * INTO ${merged_name.replace(/\s/g, '')} FROM table_source RIGHT JOIN table_${table_name.replace(/\s/g, '')} ON table_source.[Concept Name] = table_${table_name.replace(/\s/g, '')}.[Concept Name]`)
- }
+
  
  
- var dbFlag = true;
+ var dbFlag = true; // GLOBAL - First time running the app should create the table, if not ignore
  
  function NikeAirtableMergerApp() {
+ /**
+  * Main routine for performing ETL.
+  *
+  *
+  * @since      3.23.2022
+  * @access     public
+  *
+  */ 
      const base = useBase();
  
      // Configured list of teams to map against source data
@@ -225,7 +365,7 @@
              var merged = joinTables(left_side.tempTable, right_side.tempTable, left_key, right_key, all_fields);
              if (merged) {
                  var name = team_table + ' Merged Table';
-                 joinTablesinDB('source', team_table, name)
+                 joinTablesinDB( team_table, name)
                  createMergedTable(name, all_fields, left_key);
                  const mergedTable = base.getTableByNameIfExists(name);
                  if (mergedTable) {
@@ -234,7 +374,6 @@
                      var staleRecords = findStaleRecords(joinableTable.tempTable, merged);
                      insertMergedRecords(unmergedRecords, name);
                      deleteStaleRecordsFromTable(staleRecords, mergedTable, name);
-                     // updateRecordsInTable(staleRecords, mergedTable, name);
                  }
              }
          }
